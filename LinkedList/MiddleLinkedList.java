03e7356fef34f34c190b8155082261b8d24570d2
Amit Diwakar <143426340+amitsitare@users.noreply.github.com>
2025-12-03 21:10:41 +0530

    Add files via upload

---
+++ LinkedList/MiddleLinkedList.java
@@ -0,0 +1,47 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class MiddleLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        MiddleLinkedList obj = new MiddleLinkedList();
+        ListNode mid = obj.middleNode(head);
+
+        System.out.println("Middle Node Value: " + mid.val);
+    }
+}
